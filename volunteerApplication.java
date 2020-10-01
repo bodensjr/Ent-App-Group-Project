2f9d30a061ac65cc4c2ce031f8439282bee80f01
bodensjr <70085015+bodensjr@users.noreply.github.com>
2020-10-01 16:16:19 -0400

    Update volunteerApplication.java

--- volunteerApplication.java
+++ volunteerApplication.java
@@ -1,1 +1,12 @@
+package com.volunteerSite.enterprise;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+Public class volunteerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(volunteerApplication.class, args);
+  }
+}
