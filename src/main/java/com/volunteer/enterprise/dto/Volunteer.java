63c37817ab56e0580c786223c19534e84ba08151
muhammus-UC <49666001+muhammus-UC@users.noreply.github.com>
2020-11-03 00:06:14 -0500

    Company & Volunteer DTO - Implemented a more readable toString() than Lombok provides.

--- src/main/java/com/volunteer/enterprise/dto/Volunteer.java
+++ src/main/java/com/volunteer/enterprise/dto/Volunteer.java
@@ -9,5 +9,13 @@ class Volunteer {
     private String volunteerUser;
     private String volunteerPass;
     public String testField;
+
+    public String toString() {
+        return "Volunteer - " +
+                " ID: " + volunteerID +
+                " Email: " + volunteerEmail +
+                " User: " + volunteerUser +
+                " Password: " + volunteerPass;
+    }
 }
 
