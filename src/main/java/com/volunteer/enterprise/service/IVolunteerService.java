bb36c5ef2e20bccc975a77dd5715402e475862f6
muhammus-UC <49666001+muhammus-UC@users.noreply.github.com>
2020-11-03 00:08:16 -0500

    Volunteer Service - Properly implemented Stub and updated Interface.

--- src/main/java/com/volunteer/enterprise/service/IVolunteerService.java
+++ src/main/java/com/volunteer/enterprise/service/IVolunteerService.java
@@ -4,15 +4,11 @@ import com.volunteer.enterprise.dto.Volunteer;
 import java.util.List;
 
 public interface IVolunteerService {
-
-    Volunteer fetch(int id);
-
     void delete(int parseInt) throws Exception;
 
-    Volunteer fetchById (int id);
+    Volunteer fetchById(int id);
+
     List<Volunteer> fetchAll();
 
     Volunteer save(Volunteer volunteer) throws Exception;
-
-
 }
