48169ecccd34716b82aff97e3757c3573080af28
bodensjr <70085015+bodensjr@users.noreply.github.com>
2020-11-21 19:32:35 -0500

    Update Applicant.java

--- src/main/java/com/volunteer/enterprise/dto/Applicant.java
+++ src/main/java/com/volunteer/enterprise/dto/Applicant.java
@@ -3,16 +3,20 @@ package com.volunteer.enterprise.dto;
 import lombok.Data;
 
 public @Data class Applicant {
-    private int applicantID;
+    private String applicantName;
+    private String applicantProf;
+    private String applicantCred;
+    private String applicantPhone;
     private String applicantEmail;
-    private String applicantUser;
-    private String applicantPass;
+    private String applicantDesc;
 
     public String toString() {
         return "Applicant - " +
-                " ID: " + applicantID +
+                " Name: " + applicantName +
+                " Type of Job: " + applicantProf +
+                " Credentials: " + applicantCred +
+                " Phone Number: " + applicantPhone +
                 " Email: " + applicantEmail +
-                " User: " + applicantUser +
-                " Password: " + applicantPass;
+                " Description: " + applicantDesc;
     }
 }
