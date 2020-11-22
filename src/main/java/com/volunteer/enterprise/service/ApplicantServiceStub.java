b452e515bbf6e8bbd4d6f621dc54e32a4e883e2d
bodensjr <70085015+bodensjr@users.noreply.github.com>
2020-11-21 19:51:38 -0500

    Update ApplicantServiceStub.java

--- src/main/java/com/volunteer/enterprise/service/ApplicantServiceStub.java
+++ src/main/java/com/volunteer/enterprise/service/ApplicantServiceStub.java
@@ -1,41 +1,41 @@
 package com.volunteer.enterprise.service;
 
-import com.volunteer.enterprise.dao.ICompanyDAO;
-import com.volunteer.enterprise.dto.Company;
+import com.volunteer.enterprise.dao.IApplicantDAO;
+import com.volunteer.enterprise.dto.Applicant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class CompanyServiceStub implements ICompanyService {
+public class ApplicantServiceStub implements IApplicantService {
     @Autowired
-    private ICompanyDAO companyDAO;
+    private IApplicantDAO applicantDAO;
 
-    public CompanyServiceStub() {}
+    public ApplicantServiceStub() {}
 
-    public CompanyServiceStub(ICompanyDAO companyDAO) {
-        this.companyDAO = companyDAO;
+    public ApplicantServiceStub(IApplicantDAO applicantDAO) {
+        this.applicantDAO = applicantDAO;
     }
 
     @Override
     public void delete(int id) throws Exception {
-        companyDAO.delete(id);
+        applicantDAO.delete(id);
     }
 
     @Override
-    public Company fetchById(int id) {
-        Company foundCompany = companyDAO.fetch(id);
-        return foundCompany;
+    public Applicant fetchById(int id) {
+        Applicant foundApplicant = applicantDAO.fetch(id);
+        return foundApplicant;
     }
 
     @Override
-    public List<Company> fetchAll() {
-        return companyDAO.fetchAll();
+    public List<Applicant> fetchAll() {
+        return applicantDAO.fetchAll();
     }
 
     @Override
-    public Company save(Company company) throws Exception {
-        return companyDAO.save(company);
+    public Applicant save(Applicant applicant) throws Exception {
+        return applicantDAO.save(applicant);
     }
 }
