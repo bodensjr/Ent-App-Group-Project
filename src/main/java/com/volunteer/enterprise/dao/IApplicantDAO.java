60a8f8344bec82c17e73aa64cfaff35bec4c17f7
bodensjr <70085015+bodensjr@users.noreply.github.com>
2020-11-23 21:45:27 -0500

    Update IApplicantDAO.java

--- src/main/java/com/volunteer/enterprise/dao/IApplicantDAO.java
+++ src/main/java/com/volunteer/enterprise/dao/IApplicantDAO.java
@@ -8,6 +8,9 @@ public interface IVolunteerDAO {
     Volunteer save(Volunteer volunteer) throws Exception;
 
     List<Volunteer> fetchAll();
+    
+    //get the values of the variables in Company to display in HTML
+    document.getElementById("company").innerHTML = fetchAll();
 
     Volunteer fetch(int id);
 
